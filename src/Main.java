be6ea909657c20a7888690e6bbb79d3d1b6d56be
Arsenii55 <arseka2512@gmail.com>
2023-07-16 10:34:45 +0200

    6 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.lang.reflect.Field;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ClassNotFoundException {
+        try (Ressurs ressurs = new Ressurs()){
+            throw new RuntimeException();
+        } catch (Exception e) {
+            throw new ClassNotFoundException();
+        }
+    }
+}
+class Ressurs implements AutoCloseable{
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("close method");
+        throw new RuntimeException();
     }
 }
